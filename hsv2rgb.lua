a45a9f144a57d343a3f8ad9adefaf8dd57aacae1
janitz <janis-d@web.de>
2015-12-02 13:01:14 +0100

    initial commit
    Converts an HSV color value to RGB.
    Conversion formula adapted from http://en.wikipedia.org/wiki/HSV_color_space.
    Assumes h, s, v are contained in the set [0, 1]
    Returns r, g, b in the set [0, 255].
    @param   Number  h       The hue
    @param   Number  s       The saturation
    @param   Number  v       The value
    @return    r,g,b           The RGB representation

---
+++ hsv2rgb.lua
@@ -0,0 +1,21 @@
+hsv2Rgb = function(h,s,v)
+    local r,g,b
+    local i=floor(h*6)
+    local f=h*6-i
+    local p=v*(1-s)
+    local q=v*(1-f*s)
+    local t=v*(1-(1-f)*s)
+    i=i%6
+    if i==0 then r,g,b=v,t,p
+    elseif i==1 then r,g,b=q,v,p
+    elseif i==2 then r,g,b=p,v,t
+    elseif i==3 then r,g,b=p,q,v
+    elseif i==4 then r,g,b=t,p,v
+    elseif i==5 then r,g,b=v,p,q
+    end
+    return r*255,g*255,b*255
+end
+
+floor = function(i)
+    return i-(i%1)
+end
